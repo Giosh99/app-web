7ede1e356a7dbd9efb9c670798d0e2d070a0d027
master <calgarogiosue@outlook.com>
2018-04-04 16:16:27 +0200

    commit

---
+++ header.php
@@ -0,0 +1,15 @@
+
+<div class="flex-row nav-bar d-flex align-items-center text-center" id="header" style="color: #f0f1ee;font-size:2vh;">
+    <div class="col-1 ml-0 ml-0 p-0 house button h-100"><img src="icons/house.svg"/></div>
+    <div class="col-1 ml-0 p-0 text-truncate d-block"> <!-- boh-iconnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn --></div>
+    <div class="col-1 ml-auto bell button h-100" style="overflow:hidden"><img src="icons/bell.svg"/></div>
+    <button class="col-1 ml-0 p-0 user button h-100 dropdown-toggle" style="border-radius:0" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" ><img src="icons/user.svg" style="width:70%"/></button>
+
+<!-- ----------------------- menu of user button ------------------------------>
+<div class="dropdown-menu dropdown-menu-right">
+    <a class="dropdown-item" href="#" id="enter-choice">Enter in your profile</a>
+    <div class="dropdown-divider"></div>
+    <a class="dropwdown-item align-self-center ml-4" href="#due">Log out</a>
+</div>
+<!-- ---------------------------------------------------------------------------->
+        </div>
